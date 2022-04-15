dda08e6ff5f808eadbd630b144016f7dcbefae86
Michael Montanaro <michaelmontanaro@Michaels-MacBook-Pro.local>
2022-04-15 18:10:49 -0400

    Adding setup.sh file and editing README.md with instructions

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# setup
-Instructions on initial Macbook setup
+# Setup - Repository for easy initialization of new Macbooks
+
+1. Setup github ssh keys
+2. Clone repository
+3. Run `sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"`
+4. Run `chmod +x setup.sh` in directory with setup.sh file
+5. Run `./setup.sh`
+6. Sit back and enjoy the newly setup computer!
---
+++ setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+echo Checking for zsh...
+if [ -e /bin/zsh ]
+then
+  echo Terminal running zsh successfully 
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  brew install htop
+  brew install gotop
+  brew install bat
+  echo Installing oh-my-zsh and required texts
+  git clone https://github.com/powerline/fonts.git --depth=1
+  cd fonts && ./install.sh
+  cd .. && rm -rf fonts
+  echo Moving oh-my-zsh ~/.zshrc file to ~/.zshrc.oh-my-zsh
+  mv ~/.zshrc ~/.zshrc.oh-my-zsh
+  if [ ! -e ~/.zshrc.pre-oh-my-zsh]
+  then
+    echo No prior ~/.zshrc file found
+    echo source ~/zshrc.oh-my-zsh > ~/.zshrc
+  else
+    echo -e "source ~/.zshrc.oh-my-zsh\nsource ~/.zshrc.pre-oh-my-zsh > ~/.zshrc
+  fi
+  echo All set up!
+fi
+
